7e34962b93eca111d23b57019c4029cd78652464
gregxsunday <gregxsunday@gmail.com>
2021-07-07 12:44:54 +0200

    added M01L05

---
+++ m1/lekcja5.py
@@ -0,0 +1,7 @@
+import socket
+
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysocket.settimeout(1)
+address = ('8.8.8.8', 443)
+result = mysocket.connect_ex(address)
+mysocket.close()

0f889bc410878b5a54d2682764c40a21fc24ef96
gregxsunday <gregxsunday@gmail.com>
2021-07-07 12:41:51 +0200

    added M01L04

---
+++ m1/lekcja4.py
@@ -0,0 +1,16 @@
+fruit1 = 'jablko'
+fruit2 = 'cytryna'
+fruit3 = 'arbuz'
+
+fruits = ['jablko', 'cytryna', 'arbuz']
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+
+fruits.append('winogrono')
+print(fruits)
+last = fruits.pop()
+
+print(fruits)
+print(last)

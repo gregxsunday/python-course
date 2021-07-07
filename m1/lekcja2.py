d1e77cb83d4fbaca622a8cae1485a5e801b198ff
gregxsunday <gregxsunday@gmail.com>
2021-07-07 12:37:42 +0200

    added M01L02

---
+++ m1/lekcja2.py
@@ -0,0 +1,51 @@
+# print(789 + 456)
+# print(789 - 456)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# liczba = 789
+
+# print(liczba + 456)
+# print(liczba - 456)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print('string')
+# print("string")
+
+# zmienna = "napis"
+# print(type(zmienna))
+zmienna = 12
+print(type(zmienna))
+
+zmienna_string = str(12)
+# zmienna_string = "12"
+print(type(zmienna_string))
+print(zmienna_string)
+
+
+

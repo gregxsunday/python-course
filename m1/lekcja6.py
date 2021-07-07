8d84ecf0f6d7b005b41ea63643fabb68ca097810
gregxsunday <gregxsunday@gmail.com>
2021-07-07 12:58:53 +0200

    added M01L06

---
+++ m1/lekcja6.py
@@ -0,0 +1,24 @@
+true = True
+false = False
+# True = 1
+
+print('1 == 2', 1 == 2)
+print('1 != 2', 1 != 2)
+print('1 > 2', 1 > 2)
+print('1 >= 2', 1 >= 2)
+print('1 < 2', 1 < 2)
+print('1 <= 2', 1 <= 2)
+print('"string" == "napis"', "string" == "napis")
+print('"string" != "napis"', "string" != "napis")
+
+print('1 == "1"', 1 == "1")
+print('1 == int("1")', 1 == int("1"))
+print('str(1) == "1"', str(1) == "1")
+
+print(1 > 2 or 4 > 3)
+print(1 > 2 and 4 > 3)
+
+print(1 < 2 and 4 > 3)
+
+print(not 1 > 2 and 4 > 3)
+

ca686fc2d944f959010a6d660a2b4f1d9e007a00
gregxsunday <gregxsunday@gmail.com>
2021-07-07 12:37:53 +0200

    added M01L03

---
+++ m1/lekcja3.py
@@ -0,0 +1,16 @@
+print(123)
+
+import time
+time.sleep(5)
+
+print(abs(-50))
+
+number = -50
+number_abs = abs(number)
+
+a = 34
+b = 43
+greater = max(a, b)
+
+napis = 'python'
+print(napis.upper())

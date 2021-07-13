38f1e0808dd05d55bb44e9179127cd87d622c488
gregxsunday <gregxsunday@gmail.com>
2021-07-13 11:24:00 +0200

    added M01L10

---
+++ m1/lekcja10.py
@@ -0,0 +1,62 @@
+import socket
+
+def is_port_open(port, ip):
+    mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysocket.settimeout(1)
+
+    address = (ip, port)
+
+    result = mysocket.connect_ex(address)
+    mysocket.close()
+
+    if result == 0:
+        return True
+    else:
+        return False
+
+
+server = {
+    1: 'closed',
+    2: 'closed',
+    3: 'open'
+}
+
+
+# server = {
+#     1: {
+#         'status': 'closed', 
+#         'service': None
+#         },
+#     2: {
+#         'status': 'closed', 
+#         'service': None
+#         },
+#     3: {
+#         'status': 'open', 
+#         'service': 'Apache'
+#         },
+# }
+
+for port in range(0, 1024):
+    if is_port_open(port, '8.8.8.8'):
+        server[port] = 'open'
+        # print('port', port, 'jest otwarty')
+    else:
+        server[port] = 'closed'
+        # print('port', port, 'jest zamknięty')
+
+# print('# Porty w słowniku')
+# for port in server:
+#     print(port)
+
+# print('\n# Porty w słowniku używając .keys()')
+# for port in server.keys():
+#     print(port)
+
+# print('\n# Statusy w słowniku używając .values()')
+# for status in server.values():
+#     print(status)
+
+# print('\n# Porty i statusy w słowniku używając .items()')
+# for port, status in server.items():
+#     print(port, status)

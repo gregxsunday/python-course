53f47c1daf040ec81b2219127e76b85f40f8c358
gregxsunday <gregxsunday@gmail.com>
2021-07-14 12:27:23 +0200

    added M02L07

---
+++ m2/lekcja07.py
@@ -0,0 +1,29 @@
+from os import stat
+
+
+if __name__ == '__main__':
+    with open('ffuf.log', 'r') as infile:
+        logs = infile.read()
+
+    for log in logs.split('\n'):
+        log_split = log.split(' ')
+        print(f'{log_split=}')
+        log = log.replace(', ', ' ')
+        log_split = log.split(' ')
+        print(f'{log_split=}')
+
+        path = log_split[0]
+        status_code = log_split[2]
+
+        wp_admin = path.find('wp-admin')
+
+        if wp_admin != -1 and status_code == '200':
+            print(f'{path=}\twp-admin found at index: {wp_admin}')
+        else:
+            print(f'{path=}\twp-admin not found. {wp_admin=}')
+
+        # js = path.find('.js')
+        
+        # if wp_admin != -1 and js == -1 and status_code == '200':
+        #     print(f'{path=}')
+        

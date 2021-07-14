fa4ead462b6fdf570e6012803fda1e43e6ab2db3
gregxsunday <gregxsunday@gmail.com>
2021-07-14 11:47:17 +0200

    added M02L06

---
+++ m2/lekcja06.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    with open('ffuf.log', 'r') as infile:
+        logs = infile.read()
+
+    log = logs.split('\n')[0]
+    print(f'{log=}')
+
+    log_split = log.split(' ')
+    print(f'{log_split=}')
+    print(f'{log_split[0]=}')
+    print(f'{log_split[2:4]=}')
+    print(f'{log_split[:4]=}')
+    print(f'{log_split[2:-2]=}')
+    print(f'{log_split[:-1]=}')
+    print(f'{log_split[:]=}')
+    print(f'{log_split[-5:5]=}')

4df4fd734d890263beff88b6b8bf9d462b8bbff0
gregxsunday <gregxsunday@gmail.com>
2021-07-14 12:43:35 +0200

    added M02L09

---
+++ m2/lekcja09.py
@@ -0,0 +1,22 @@
+import re
+
+if __name__ == '__main__':
+    with open('ffuf.log', 'r') as infile:
+        logs = infile.read()
+
+    for log in logs.split('\n'):
+        log = log.replace(', ', ' ')
+        log_split = log.split(' ')
+
+        path = log_split[0]
+        status_code = log_split[2]
+        size = log_split[4]        
+
+        js = re.search('\.js$', path)
+        if js:
+            with open('js.txt', 'a') as outfile:
+                print(path, status_code, size)
+                # print(path, status_code, size, file=outfile)
+                # print(f'{path}|{status_code}|{size}', file=outfile)
+                # print(path, status_code, size, file=outfile, sep='|')
+                # print(path, status_code, size, file=outfile, sep='|', end=';\n')

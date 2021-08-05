7c9ea41d8cc86fc69a7c420f123a37b1d14a6ce0
gregxsunday <gregxsunday@gmail.com>
2021-08-05 08:27:34 +0200

    added M05L03

---
+++ m5/lekcja03.py
@@ -0,0 +1,12 @@
+from hashlib import sha256
+
+if __name__ == '__main__':
+    data = 'xss'
+
+    print(sha256(data))
+    print(sha256(data.encode()))
+    print(sha256(data.encode()).hexdigest())
+
+    print(sha256('xss').hexdigest())
+    print(sha256('xss'.encode()).hexdigest())
+    print(sha256(b'xss').hexdigest())

